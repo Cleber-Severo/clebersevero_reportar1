692a346193c3c6e3541a4bf800c2a183c666be9a
Cléber Severo <clebersevero1998@gmail.com>
2023-08-30 18:29:54 -0300

    feat: checkbox styled

--- src/components/Header/Checkbox/index.jsx
+++ src/components/Header/Checkbox/index.jsx
@@ -4,21 +4,23 @@ import './Checkbox.scss'
 const Checkbox = () => {
     return (
         <div className='checkbox-form'>
+            <label className='checkbox-item'>
+                <input type="checkbox"  />
+                O profissional não compareceu
+                <span></span>
+            </label>
 
-            <div>
-                <input type="checkbox" name='options' id='checkbox_1' />
-                <label htmlFor="checkbox_1">O profissional não compareceu</label>
-            </div>
+            <label className='checkbox-item'>
+                <input type="checkbox"  />
+                O profissional não prestou um bom serviço
+                <span></span>
+            </label>
 
-            <div>
-                <input type="checkbox" name='options' id='checkbox_2' />
-                <label htmlFor="checkbox_2">O profissional não prestou um bom serviço</label>
-            </div>
-
-            <div>
-                <input type="checkbox" name='options' id='checkbox_3' />
-                <label htmlFor="checkbox_3">Outro.</label>
-            </div>
+            <label className='checkbox-item'>
+                <input type="checkbox"  />
+                Outro.
+                <span></span>
+            </label>
         </div>
     )
 }
