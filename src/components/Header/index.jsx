bb4255658aad014b5ded20c0132cd8449419a9b3
Cléber Severo <clebersevero1998@gmail.com>
2023-08-30 17:18:15 -0300

    feat: header component styled

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { HiOutlineArrowLeft } from 'react-icons/hi'
 import './Header.scss'
 
 const Header = () => {
   return (
     <header>
-        <span>icon</span>
-        <h2>Reportar</h2>
+      <HiOutlineArrowLeft className='arrow-icon'/>
+      <h2>Reportar</h2>
     </header>
   )
 }
