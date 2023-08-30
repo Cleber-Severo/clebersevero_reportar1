a4eb06d373f9be14b06cbf98a69a34e78c104c39
Cléber Severo <clebersevero1998@gmail.com>
2023-08-30 16:14:43 -0300

    feat: header component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.scss'
+import Header from './components/Header'
 
 function App() {
   return (
-    <>
     <div className='app'>
+      <Header />
 
-      <p>Hello World</p>
     </div>
-    </>
+
   )
 }
 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <span>icon</span>
+        <h2>Reportar</h2>
+    </header>
+  )
+}
+
+export default Header
