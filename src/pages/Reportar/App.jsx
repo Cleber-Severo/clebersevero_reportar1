f4929c46efa1cdf74e71ecf02c89921b7b866d55
Cléber Severo <clebersevero1998@gmail.com>
2023-08-31 10:44:11 -0300

    feat: button component created

--- src/pages/Reportar/App.jsx
+++ src/pages/Reportar/App.jsx
@@ -2,6 +2,7 @@ import Checkbox from '/src/components/Checkbox'
 import Header from '/src/components/Header'
 
 import './App.scss'
+import Button from '../../components/Button'
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
         <Checkbox />
         <input type="text" placeholder='Título' />
         <textarea name="" id="" cols="10" rows="10" placeholder='Especifique o que houve'></textarea>
-
+        <Button>
+          Reportar
+        </Button>
       </form>
     </div>
 
