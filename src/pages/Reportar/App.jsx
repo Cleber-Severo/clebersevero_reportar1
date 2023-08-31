c6a405c95b3f709feea1589a7d9fa3404989fd26
Cléber Severo <clebersevero1998@gmail.com>
2023-08-31 11:08:11 -0300

    feat: error handling function and error message to empty fields

--- src/pages/Reportar/App.jsx
+++ src/pages/Reportar/App.jsx
@@ -3,11 +3,21 @@ import Header from '/src/components/Header'
 
 import './App.scss'
 import Button from '../../components/Button'
+import { useState } from 'react'
 
 function App() {
+  const[error, setError] = useState('');
+  const[validation, setValidation] = useState('');
 
   function validateForm(e) {
     e.preventDefault();
+
+    if (validation == "") {
+      setError('error');
+      setTimeout(() => { setError(''); }, 2500)
+      return;
+    }
+    setError('')    
   }
 
   return (
@@ -15,8 +25,15 @@ function App() {
         <Header />
       <form className='report-form' action="" onSubmit={validateForm} >
         <Checkbox />
-        <input type="text" placeholder='Título' />
-        <textarea name="" id="" cols="10" rows="10" placeholder='Especifique aqui o que houve'></textarea>
+        <input type="text" placeholder='Título' className={error} onChange={e => setValidation(e.target.value) } />
+        <textarea 
+          cols="10" 
+          rows="10" 
+          placeholder='Especifique aqui o que houve'
+          className={error}
+          onChange={e => setValidation(e.target.value)} 
+        ></textarea>
+        <small className={error}>*Preencha todos os campos</small>
         <Button>
           Reportar
         </Button>
